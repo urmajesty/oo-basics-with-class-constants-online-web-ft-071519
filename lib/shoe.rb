680a6e46f73a06deba6849740926b4600dd763b3
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 17:56:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -19,13 +19,13 @@ class Shoe
       BRANDS << @brand
     end
   end
+end
 
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-end
 end
       BRANDS << @brand
     end
