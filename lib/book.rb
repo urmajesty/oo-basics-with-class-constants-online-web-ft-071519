8cc79c4a38b99dab340a7534afe4f78c2de5a9f2
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 17:07:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count, 
+  attr_reader :title :genre
   
   GENRES =[]
 
@@ -11,5 +11,9 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+def genre(genre)
+  @genre= genre
+end
 
 end
