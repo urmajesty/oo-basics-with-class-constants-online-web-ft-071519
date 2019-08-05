53613fcd1e505cf861b658a45d88ec7d12e94263
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 17:45:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition 
   attr_reader :brand
   
-  BRANDS = []
-    
-  BRANDS << brand
+  B
 end
 
-  def cobble
+  def cobble()
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
