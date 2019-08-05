63060fc0c6968efb4da34c2351e9e8315e2f6e03
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 17:36:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 require "pry"
 class Shoe
-  attr_accessor :color, :size, :material, :condition :brand
-  attr_reader 
+  attr_accessor :color, :size, :material, :condition 
+  attr_reader :brand
   
   BRANDS = []
 
